c8e48f61f933e513ee371a93d206e83475578aba
Stefan Loeffler <s.loeffler@custom-co.de>
2019-08-04 21:17:09 +0200

    switched tests to Directory.Build.props

--- src/Test/Core.CodeGeneration.Modelling.ClassModel/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Test.Core.CodeGeneration.Modelling.ClassModel")]
-[assembly: AssemblyDescription("Automated tests for the Core.CodeGeneration.Modelling.ClassModel assembly")]
-[assembly: Guid("9ebe1c69-1c5e-4d37-a0b7-00d21ab6cb9b")]
--- src/Test/Core.CodeGeneration.Modelling/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Test.Core.CodeGeneration.Modelling")]
-[assembly: AssemblyDescription("Automated tests for the Core.CodeGeneration.Modelling assembly")]
-[assembly: Guid("9ebe1c69-1c5e-4d37-a0b7-00d21ab6cb9a")]
