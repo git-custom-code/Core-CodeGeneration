fae921f0790f9a40aa8d4406ef2b1aa816aa751d
Stefan Loeffler <s.loeffler@custom-co.de>
2019-08-04 20:59:49 +0200

    switched to Directory.Build.props

--- src/Core/CodeGeneration.Modelling/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using CustomCode.Core.Composition;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Core.CodeGeneration.Modelling")]
-[assembly: AssemblyDescription("C# code generation via *.csx scripts")]
-[assembly: Guid("9ebe1c69-1c5e-4d37-a0b7-00d21ab6cb8a")]
-
-[assembly: IocVisibleAssembly]
--- src/Core/CodeGeneration.Scripting/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using CustomCode.Core.Composition;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Core.CodeGeneration.Scripting")]
-[assembly: AssemblyDescription("C# code generation via *.csx scripts")]
-[assembly: Guid("9ebe1c69-1c5e-4d37-a0b7-00d21ab6cb8c")]
-
-[assembly: IocVisibleAssembly]
